6aa3608bb784c20d54046adefe9bb43964e46eaa
Constanntine <84543782+Constanntine001@users.noreply.github.com>
2025-01-25 18:02:18 -0300

    Ajustes tamanho player

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -21,7 +21,7 @@ func _on_death_area_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("PLAYER"):
 		return
 	
-	await get_tree().create_timer(6).timeout
+	await get_tree().create_timer(4).timeout
 	BubblePoints.remove_bubble_points(BubblePoints.BubblePenalties.DEATH, body.get_parent().id)
 	BubblePoints.update_ui()
 	body.linear_velocity = Vector3.ZERO
