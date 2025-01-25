0d54be828ca100fcbcf967d5f6ff39d6e1fef503
Pedro Marangon <pedrommarangon@gmail.com>
2025-01-25 19:55:55 -0300

    .

--- objects/player/Player.gd
+++ objects/player/Player.gd
@@ -11,27 +11,45 @@ class_name Player
 @onready var rigid_body_3d :RigidBody3D = $RigidBody3D
 
 func _ready():
+	$HamsterParent.scale = Vector3.ONE * 0.5
+	%Score.scale = Vector3.ONE * 1.5
 	%HamsterMesh.set_surface_override_material(0, material)
 
 func _physics_process(delta):
 	$FloorCheck.global_transform.origin = $RigidBody3D.global_transform.origin
+	var joystick_input = Vector2(Input.get_joy_axis(id, JOY_AXIS_LEFT_X),Input.get_joy_axis(id, JOY_AXIS_LEFT_Y))
 	
-	$RigidBody3D.linear_velocity.x -= Input.get_joy_axis(id, JOY_AXIS_LEFT_X)*(rolling_force*delta)
-	$RigidBody3D.linear_velocity.z -= Input.get_joy_axis(id, JOY_AXIS_LEFT_Y)*(rolling_force*delta)
-	
-	if can_jump and Input.is_joy_button_pressed(id, JOY_BUTTON_A) and $FloorCheck.is_colliding():
-		$RigidBody3D.apply_central_impulse(Vector3(0, 1, 0) * jumping_force)
+	update_position(joystick_input, delta)
 	
 	$HamsterParent.global_transform.origin = $RigidBody3D.global_transform.origin + (Vector3.DOWN * 0.25)
 	
+	update_hamster_y_rotation(joystick_input)
 	
-	var target_rotation = atan2(-Input.get_joy_axis(id, JOY_AXIS_LEFT_X), -Input.get_joy_axis(id, JOY_AXIS_LEFT_Y))  # Get angle in radians
-	var target_degrees = rad_to_deg(target_rotation)  # Convert to degrees
-	$HamsterParent.rotation_degrees = Vector3(0,target_degrees,0)
+	update_hamster_running_rot(delta, joystick_input)
 	
 	update_player_score()
-	
+
+
+func update_position(joystick :Vector2,delta:float):
+	$RigidBody3D.linear_velocity.x -= joystick.x*(rolling_force*delta)
+	$RigidBody3D.linear_velocity.z -= joystick.y*(rolling_force*delta)
+	if can_jump and Input.is_joy_button_pressed(id, JOY_BUTTON_A) and $FloorCheck.is_colliding():
+		$RigidBody3D.apply_central_impulse(Vector3(0, 1, 0) * jumping_force)
+
+
+func update_hamster_y_rotation(joystick :Vector2):
+	var target_rotation = atan2(-joystick.x, -joystick.y)  # Get angle in radians
+	var target_degrees = rad_to_deg(target_rotation)  # Convert to degrees
+	$HamsterParent.rotation_degrees = Vector3(0,target_degrees,0)
+
+
+func update_hamster_running_rot(delta:float, input:Vector2):
+	const MULTIPLIER = 200
+	var run_strength = input.length() * $RigidBody3D.linear_velocity.length()
+	var run_angle = 10
+	%HamsterMesh.rotation_degrees = Vector3.RIGHT * -sin(delta * run_strength * MULTIPLIER) * run_angle
+
 func update_player_score():
 	var number = BubblePoints.get_points_by_id(id)
 	var padded = "%03d" % number
-	$"RigidBody3D/Score".text = "P" + str(id + 1) + " " + "BP: " + str(padded)
+	%Score.text = "P" + str(id + 1) + " " + "BP: " + str(padded)
